8d9e9feb6f4bcd88fc00ef91e7dcaa8e3750dbc1
Jnaneshwar Das <jnaneshwar.das@gmail.com>
2017-10-31 20:40:22 -0400

    first commit for leader-follower openuav simulation

---
+++ leader-follower/simulation/run_this.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+source /root/.profile 
+source /simulation/inputs/parameters/swarm.sh
+
+echo "Setup..."
+python /simulation/inputs/setup/testCreateUAVSwarm.py $num_uavs &
+sleep 15
+python /simulation/inputs/setup/testArmAll.py $num_uavs &
+
+
+echo "Controllers..."
+python /simulation/inputs/controllers/test_1_Loop.py 5 1 1 0 &> /dev/null & 
+python /simulation/inputs/controllers/test_2_Follow.py 2 1 2 &> /dev/null &
+python /simulation/inputs/controllers/test_2_Follow.py 3 2 3 &> /dev/null &
+
+echo "measures"
+python /simulation/inputs/measures/measureInterRobotDistance.py 2 1 &> /dev/null &
+
+
+tail -f /dev/null
+
+
+
